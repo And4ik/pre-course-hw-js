c48fd58b5a2f352590cb1469eba484e0ffc5d443
And4ik <And4ik11@gmail.com>
2024-04-13 21:46:24 +0200

    02-5

---
+++ lesson-02/05.js
@@ -0,0 +1,11 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+let driverLesson = {
+    name: "Ivan",
+    surname: "Petrov",
+};
+console.log(passport.name)
+console.log(driverLesson.name)
+
