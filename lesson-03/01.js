59192a3edd0d83913a5f5f8c1a448ce0f258f6d2
And4ik <And4ik11@gmail.com>
2024-04-18 22:09:47 +0200

    03-1 filter, join, includes

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,9 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+
+    const filteredColors = colors.filter((color, index) => ![0,2,5].includes(index) )
+    return filteredColors.join("-");
+}
+
+console.log(createColorString());
